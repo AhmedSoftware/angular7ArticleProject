d898210e12063ae941c5f04d503820ccf448c92c
ahmedSoftware <aali25551@gmail.com>
2019-05-27 00:24:02 +0200

    observable subscripte subject

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { interval,Subscription} from 'rxjs';
+import { LoginService } from '../login/login.service';
 
 
 @Component({
@@ -8,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private loginService:LoginService) { }
+  secondes:number;
+  counterSubscription: Subscription;
 
   ngOnInit() {
+    
+      const counter =  interval(1000);
+      this.counterSubscription=counter.subscribe(
+        (value) => {
+          this.secondes = value;
+        },
+        (error) => {
+          console.log('Uh-oh, an error occurred! : ' + error);
+        },
+        () => {
+          console.log('Observable complete!');
+        }
+      );
+
+      
   }
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
+  
+
+  
 
 }
